58ac9679f52e6388b8f867e27ba9634eb9412329
Cristian Sepulveda <cristiangsc@gmail.com>
2022-06-23 20:42:17 -0400

    juego 32 bit 2

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,54 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import games from './juegos.json'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    games
   },
   getters: {
+    totalGames:state=>{
+      return state.games.reduce((total, count)=>{
+        return total + parseInt(count.stock)
+      },0)
+    }
   },
   mutations: {
+    minStock:(state,codigoSeleccionado) =>{  // reducir el stock mediante un ciclo for
+      state.games.forEach((game)=>{
+        if (game.codigo == codigoSeleccionado.codigo && game.stock>0) {
+          game.stock--
+        }
+      })
+    },
+    maxStock:(state,codigoSeleccionado) =>{ // aumentar el stock mediante funciones de array/objetos
+      let cantidad = state.games.find(juego => juego.codigo == codigoSeleccionado.codigo)
+      cantidad.stock++
+    },
+    SetearColor(state, { indice, newColor }) {
+      state.games[indice].color = newColor
+    },
   },
   actions: {
+    reduceStock({commit},codigoSeleccionado){
+      return new Promise(() => {
+        commit('minStock',codigoSeleccionado)
+      })
+    },
+    addStock({commit},codigoSeleccionado){
+      return new Promise(() => {
+        commit('maxStock',codigoSeleccionado)
+      })
+    },
+    changeColor({commit, state}, { gameSelect, newColor }) {
+      console.log(newColor)
+      console.log(gameSelect)
+      const juegos = state.games
+      const indice = juegos.findIndex((juego) => juego.codigo === gameSelect.codigo)
+      console.log(indice)
+      commit('SetearColor', { indice, newColor })
+    },
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,94 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Tienda 32 Bits</h1>
+    <h2>Lista de juegos</h2>
+    <table>
+      <thead>
+      <tr>
+        <th>Código</th>
+        <th>Color</th>
+        <th>Nombre</th>
+        <th>Stock</th>
+        <th>Precio</th>
+        <th>Acciones</th>
+      </tr>
+      </thead>
+      <tbody v-for="game in games" :key="game.codigo">
+      <tr :style="{color: game.color}">
+        <td>{{ game.codigo }}</td>
+        <td>
+          <select :value="game.color" @change="cambiarColor(game, $event)">
+            <option v-for="(color, $index) in colors" :key="$index" :value="color.value">
+              {{ color.label }}
+            </option>
+          </select>
+        </td>
+        <td>{{ game.nombre }}</td>
+        <td>{{ game.stock }}</td>
+        <td>{{ game.precio }}</td>
+        <td>
+          <button @click="SumStock(game)">+</button>
+          <button @click="subStock(game)">-</button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+    <h3>Total: {{ totalGames }} juegos</h3>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import {mapState, mapGetters, mapActions} from 'vuex'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      colors: [
+        {value: 'red',   label: 'Rojo'},
+        {value: 'blue',  label: 'Azul'},
+        {value: 'green', label: 'Verde'},
+        {value: 'black', label: 'Negro'},
+      ],
+    }
+  },
+  computed: {
+    ...mapState(['games']),
+    ...mapGetters(['totalGames']),
+  },
+  methods: {
+    ...mapActions(['reduceStock', 'addStock', 'changeColor']),
+    SumStock(game) {
+      this.addStock(game)
+    },
+    subStock(game) {
+      this.reduceStock(game)
+    },
+    cambiarColor(game, event) {
+      console.log(event.target.value)
+      this.changeColor({gameSelect: game, newColor: event.target.value})
+    },
   }
 }
 </script>
+
+<style scoped>
+.container {
+  display: grid;
+  grid-template-columns: 800px;
+  justify-content: center;
+  width: 70%;
+  margin: 0 auto;
+}
+
+td {
+  border: 2px solid black;
+}
+
+th {
+  border: 2px solid black;
+}
+
+</style>
